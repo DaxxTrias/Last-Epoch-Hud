9fc46e1e14759661e512980354783a194af63153
Sean <sean.daxxtrias@gmail.com>
2025-04-24 12:23:51 -0500

    cleanup, sprite getter

--- Mod/Utils/SpriteManager.cs
+++ Mod/Utils/SpriteManager.cs
@@ -1,3 +1,5 @@
+using Mod.Cheats.Patches;
+using Mod.Utils;
 using UnityEngine;
 
 namespace Mod.Cheats.ESP
@@ -14,6 +16,16 @@ namespace Mod.Cheats.ESP
             texture.LoadImage(bytes);
             texture.Apply();
             return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+        }
+        public static Sprite GetSprite()
+        {
+            if (npcSprite == null)
+            {
+                // Assuming BaseSprites.npcmapicon is a valid Base64 string
+                string base64String = SpriteBases.npcMapIcon.ToString();
+                npcSprite = base64String.ToSprite();
+            }
+            return npcSprite;
         }
     }
 }
